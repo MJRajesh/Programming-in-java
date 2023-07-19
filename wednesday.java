48dc147cf7fc15b9a87baeb3d759ca52fbf0027b
Rajesh <101506138+MJRajesh@users.noreply.github.com>
2023-07-19 20:04:01 +0530

    Create wednesday.java

---
+++ wednesday.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class wednesday{
+    public static void main(String[] args)
+    {
+	  int k =64%7;
+switch(k)
+{
+case 1:
+System.out.println("wednesday");
+break;
+case 2:
+System.out.println("thursday");
+break;
+case 3:
+System.out.println("friday");
+break;
+case 4:
+System.out.println("saturday");
+break;
+case 5:
+System.out.println("sunday");
+break;
+case 6:
+System.out.println("monday");
+break;
+case 7:
+System.out.println("tuesday");
+break;
+}
+}
+}
