455c731e2ddd4bc54b5684fc230f9555d9f96a8a
Rajesh <101506138+MJRajesh@users.noreply.github.com>
2023-07-20 21:23:26 +0530

    Create Sumof1st5oddnumber.java

---
+++ Sumof1st5oddnumber.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+class natura{
+//first 5 odd numbers
+public static void main(String args[])
+{
+Scanner s=new.Scanner(System.in);
+System.out.print("enter the number");
+int n=s.nextInt()
+int b=n;
+while(n>10)
+{
+int first=b%10;
+int second=b/10;
+}
+System.out.println(first);
+System.out.println(second);
+}
